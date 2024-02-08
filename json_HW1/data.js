db4fe3a6c92411366f54facc61a64f164dd445b4
Alexanrd Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2024-02-08 17:50:59 +0300

    done

---
+++ json_HW1/data.js
@@ -0,0 +1,26 @@
+const dataInfo = [
+  {
+    car: "Ford",
+    model: "Fiesta",
+    car_img: "img/ford_fiesta.jpeg",
+    description:
+      "Ford Fiesta — субкомпактный автомобиль, выпускавшийся компанией Ford Motor Company. Производится в Европе, Бразилии, Аргентине, Мексике, Венесуэле, Китае, Индии, Таиланде и Южной Африке.",
+    price: "$10000",
+  },
+  {
+    car: "Lada",
+    model: "Largus",
+    car_img: "img/lada_largus.png",
+    description:
+      "LADA Largus (лат. largus; Лада Ларгус) — российский автомобиль малого класса, представляющий собой переименованный и приспособленный под российский рынок .",
+    price: "$20000",
+  },
+  {
+    car: "Tank",
+    model: "500",
+    car_img: "img/tank_500.jpg",
+    description:
+      "Tank 500, представленный Great Wall Motors, является воплощением современного дизайна в классе рамных внедорожников.",
+    price: "$300000",
+  },
+];
