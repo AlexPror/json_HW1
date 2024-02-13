20b0e9380d381be03e6fcc5408106a06014deb9f
Alexanrd Vorobjev <vorobjev.alexandr-2017@yandex.ru>
2024-02-13 19:09:39 +0300

    done

---
+++ json_HW2/data.js
@@ -0,0 +1,49 @@
+const dataProducts = `
+[
+    {
+        "id": 1,
+        "title": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52,
+        "currency": "USD",
+        "image": "img/1.png"
+    },
+    {
+      "id": 2,
+      "title": "ELLERY X M'O CAPSULE",
+      "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+      "price": 52,
+      "currency": "RUR",
+      "image": "img/2.png"
+  },
+  {
+    "id": 3,
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": 52,
+    "currency": "USD",
+    "image": "img/3.png"
+},
+{
+  "title": "ELLERY X M'O CAPSULE",
+  "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+  "price": 52,
+  "currency": "USD",
+  "image": "img/4.png"
+},
+{
+  "title": "ELLERY X M'O CAPSULE",
+  "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+  "price": 52,
+  "currency": "USD",
+  "image": "img/5.png"
+},
+{
+  "title": "ELLERY X M'O CAPSULE",
+  "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+  "price": 52,
+  "currency": "USD",
+  "image": "img/6.png"
+}
+]
+`;
---
+++ json_seminar2/data.js
@@ -0,0 +1,52 @@
+// Дан макет https://www.figma.com/file/mZwLT9fKktsWuVyfQRoST1/shop-(Copy)-(Copy)?node-id=73%3A140  в котором представлены товары на странице корзины
+// Необходимо создать файл data.js в котором создать переменную dataProducts в которую присвоить JSON данные по товарам.
+// Вам нужно самостоятельно создать JSON данные (не забыть про кавычки
+// Добавить все данные из макета, чтобы в дальнейшем по ним мы смогли создать вёрстку
+
+const dataProducts = [
+  {
+    title: "MANGO PEOPLE",
+    subtitle: "T-SHIRT",
+    price: "$300",
+    color: "Red",
+    size: "XL",
+    quantity: "2",
+    img: "img/t-shirt.jpg",
+  },
+  {
+    title: "ELLERY X M`O",
+    subtitle: "CAPSULE",
+    price: "$52",
+    color: "Black",
+    size: "XL",
+    quantity: "1",
+    img: "img/caosule.jpg",
+  },
+];
+
+// const dataProducts = `
+// [
+//     {
+//         "id": 1,
+//         "name": "t-shirt",
+//         "brand": "Mango People",
+//         "price": 300,
+//         "currency": "USD",
+//         "color": "red",
+//         "size": "XL",
+//         "quantity": 2,
+//         "image": "./images/prod_1.jpg"
+//     },
+//     {
+//         "id": 2,
+//         "name": "capsule",
+//         "brand": "Ellery X M'O",
+//         "price": 52,
+//         "currency": "USD",
+//         "color": "black",
+//         "size": "XL",
+//         "quantity": 1,
+//         "image": "./images/prod_2.jpg"
+//     }
+// ]
+// `;
